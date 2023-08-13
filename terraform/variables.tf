48ac216a5c4ca2478fd7affce2640dcf28fddfe9
angelikavilde <angelikavilde@gmail.com>
2023-08-13 19:24:46 +0100

    terraform variables added

---
+++ terraform/variables.tf
@@ -0,0 +1,47 @@
+variable "ecr_image" {
+  type = string
+}
+
+variable "execution_role" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "sec_group_id" {
+  type = string
+}
+
+variable "ecr_image2" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "host" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "port" {
+  type = string
+}
