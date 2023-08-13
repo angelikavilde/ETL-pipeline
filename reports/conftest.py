a713d0973dc92bee98537afe020c2bb868e093f5
angelikavilde <angelikavilde@gmail.com>
2023-08-13 22:11:27 +0100

    conftest file created and finilised

---
+++ reports/conftest.py
@@ -0,0 +1,29 @@
+"""File with additions for testing"""
+
+import pytest
+import pandas as pd
+
+
+@pytest.fixture
+def data():
+    """Returns a dataframe for tests"""
+    cols = ["sum_total", "truck_name"]
+    test_data = [[2,"test"],[1,"test2"]]
+    return pd.DataFrame(data=test_data,columns=cols, index=["1","2"])
+
+
+@pytest.fixture
+def transactions():
+    """Returns a dataframe for tests"""
+    cols = ["number_of_transactions", "mean", "median", "sum_total", "truck_name"]
+    test_data = [[2,5,5,6, "test1"],[1,5,3,6,"test2"]]
+    return pd.DataFrame(data=test_data,columns=cols, index=["1","2"])
+
+
+@pytest.fixture
+def returned_transactions(transactions):
+    """Returns formatted cursor returned dataframe"""
+    columns = ['sum_total',"number_of_transactions","mean","median"]
+    transactions[columns] = transactions[columns].astype(float)
+    transactions.set_index(["truck_name"])
+    return transactions
